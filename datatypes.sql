6aace4de865c9533484b54f3fe2c035ac8944b22
Baba Koodu <adeshiname@gmail.com>
2022-05-02 18:24:41 +0100

    maths

--- datatypes.sql
+++ datatypes.sql
@@ -17,5 +17,8 @@ DECLARE @myVar as TINYINT = 2
 -- small money 4 bytes 
 
 
+--Numeric is functionally equivalent to decimal
+
 Declare @myvar as decimal(7,2) -- or numeric(7,2)
+--Using (7,2) measn the vaalue can have 7 vaues all together but only 2 values after decimal
 --12345.67-- is valid for the above 
---
+++ maths.sql
@@ -0,0 +1,22 @@
+-- if it shows pink, its a function
+
+DECLARE @myvar as NUMERIC(7,2);
+
+select @myvar * 2
+select POWER(@myvar, 2)
+select SQUARE(@myvar)
+select SQRT(@myvar)
+
+GO
+
+FLOOR  - --rounds down to the nearest whole number 
+CEILING(@myvar) --rounds up to the nearest whle number 
+ROUND = -- takes it to the specifed decimal places
+
+PI() -- basic pi in mathematices 
+
+EXP(1)
+ABS() --- Absolute value take away minus sign and returns the positive value
+SIGN()
+RAND(345) --- Random value  
+
