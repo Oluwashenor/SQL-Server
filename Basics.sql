6b6b197ec79a22afbb846629b03542f4951a20e1
Baba Koodu <adeshiname@gmail.com>
2022-05-02 13:21:08 +0100

    Initial Commit

---
+++ Basics.sql
@@ -0,0 +1,8 @@
+-- The Command below is sued to create a table 
+CREATE TABLE TableName (
+    columnName int
+)
+
+-- The command below names the column MyAnswer by using the 'AS' command-- 
+SELECT 4+9 AS MyAnswer 
+
